4b2410f3645e758febb0132d6fde748a3b17acd9
aleks253545 <aleks253545@yandex.ru>
2020-01-30 14:13:33 +0300

    fix

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-let array = [0,1,2,3,4,5,5,6,7,8,9,10];
-function maxSum (array){
-    let maxSum=0,prevElement,sum ;
-    array.forEach((element,index) => {
-        if( index==0){
-            prevElement=element;
-            sum=prevElement;
-        }else{
-            if(element==prevElement+1){
-                sum+=element;
-                prevElement=element;
-            }
-            else{
-                prevElement=element;
-                sum=element;
-            }
-        }
-        if(maxSum<sum){
-            maxSum=sum;
-        }
-    });
-    return maxSum
+const mas = [0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10];
+function maxSum(array) {
+  let result = 0; let prevElement; let sum;
+  array.forEach((element, index) => {
+    if (index === 0) {
+      prevElement = element;
+      sum = prevElement;
+    } else if (element === prevElement + 1) {
+      sum += element;
+      prevElement = element;
+    } else {
+      prevElement = element;
+      sum = element;
+    }
+    if (maxSum < sum) {
+      result = sum;
+    }
+  });
+  return result;
 }
 
-console.log(maxSum(array));
+console.log(maxSum(mas));
